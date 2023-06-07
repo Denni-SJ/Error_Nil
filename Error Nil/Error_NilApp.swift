a7b788ad48796bfae9abaf0ce3a13f359b999463
Denni <DennieEom2023@gmail.com>
2023-06-07 12:09:44 +1000

    Initial Commit

---
+++ Error Nil/Error_NilApp.swift
@@ -0,0 +1,17 @@
+//
+//  Error_NilApp.swift
+//  Error Nil
+//
+//  Created by Jin O on 2023/06/07.
+//
+
+import SwiftUI
+
+@main
+struct Error_NilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
